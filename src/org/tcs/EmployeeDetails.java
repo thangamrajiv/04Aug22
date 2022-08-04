1749cdeb405e435d0230395762b50a140f927826
thangamrajiv <goldrajiv20@gmail.com>
2022-08-04 15:28:59 +0530

    Task 1

---
+++ src/org/tcs/EmployeeDetails.java
@@ -0,0 +1,24 @@
+// package
+package org.tcs;
+//class
+public class EmployeeDetails {
+private void employeeName() {
+	System.out.println("Employee name : Rajiv");
+}	
+private void employeeId() {
+System.out.println("Employee ID : 123");
+}
+public static void main(String[] args) {
+	EmployeeDetails a = new EmployeeDetails();
+	a.employeeName();
+a.employeeId();
+	
+	
+}
+
+
+
+
+
+
+}
